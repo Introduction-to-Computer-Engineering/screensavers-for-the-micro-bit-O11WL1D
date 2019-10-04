b0b15958a1a4d0fd7e6244cc1a3532e9481408a0
O11WL1D <inksdomain@gmail.com>
2019-10-03 20:58:23 -0600

    got bouncing ball screensaver to work, now I just need to implement some system which switches between screen savers

--- screen_savers.js
+++ screen_savers.js
@@ -1,25 +1,94 @@
 //if hits left wall, : left turns right, right turns left 
 //if hits right wall, left turns right, right turns left
 //if hits floor, down turns up, direction stays
-//if hits ceil, up turns down, direction stays
+//if hits ceil, up turns down, direction stays          --essentially invert varible based on wall, if(floor||ceil)(direction=!direction)
+
+//if hits corner -- reset ball 
 
 
-
-let x_axis;
-let y_axis;
+let x_axis = 3;
+let y_axis = 1;
+led.toggle(x_axis, y_axis);
 //neighboring values of direction indicate left or right
-let direction=0;
-let left_right=0; 
+let direction = true;
+//1=up
+//0=down
+let left_right = false;
+//1=left
+//0=right
 
 
-//define protocals for left/right ascent and descent
-function travel() {
-	//upwards left
-    if(direction==1 && left_right==1){}
+
+//check, then increment. 
+for (; ;) {
+    //console.log("cycle")
+    //console.log(""+x_axis);
+    //console.log(""+y_axis);
+
+
+    basic.pause(200)
+    if (y_axis == 0) { direction = (!direction) };
+    if (y_axis == 4) { direction = (!direction) };
+
+    if (x_axis == 0) { left_right = (!left_right) };
+    if (x_axis == 4) { left_right = (!left_right) };
+
+    //upwards left
+    if (direction == true && left_right == true) {
+
+        led.toggle(x_axis, y_axis);
+        y_axis += 1;
+        x_axis -= 1;
+        led.toggle(x_axis, y_axis);
+        console.log("up_left")
+    }
+
     //upwards right
-	if(direction==1 && left_right==0){}
-   	
-led.toggle(x_axis,y_axis);
-} 
+    if (direction == true && left_right == false) {
+
+        led.toggle(x_axis, y_axis);
+        y_axis += 1;
+        x_axis += 1;
+        led.toggle(x_axis, y_axis);
+
+        console.log("up_right")
+    }
+
+
+    //Downwards left
+    if (direction == false && left_right == true) {
+
+
+        led.toggle(x_axis, y_axis);
+        y_axis -= 1;
+        x_axis -= 1;
+        led.toggle(x_axis, y_axis);
+
+        console.log("down_left")
+
+    }
+    //Downwards right
+    if (direction == false && left_right == false) {
+
+
+        led.toggle(x_axis, y_axis);
+        y_axis -= 1;
+        x_axis += 1;
+        led.toggle(x_axis, y_axis);
+        console.log("down_right")
+
+    }
+
+
+    //console.log(""+direction)
+    //console.log(""+left_right)
+
+
+
+
+}
+
+
+
 
 
